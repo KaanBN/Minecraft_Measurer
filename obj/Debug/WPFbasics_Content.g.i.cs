a9004a2dfa3c72b5b51dad30ed4cce4126eb54e5
KaanBN <kaan.mlnym@hotmail.com>
2020-06-11 22:06:36 +0300

    -Added a credit page -Added a History page that under construct -Drafted the pages

--- obj/Debug/WPFbasics_Content.g.i.cs
+++ obj/Debug/WPFbasics_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("tecaher.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("pages/underconstruction.gif")]
 
 
